d2d5b01f48e137c743114894dc10bce6c8e3ee21
Sebastian Pape <s@pape.io>
2021-03-04 10:40:29 +0100

    start donations

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+import Donation from './Donation';
 import Payment from './Payment';
 import Sale from './Sale';
-import Swap from './Swap';
 import Selector from './Selector';
+import Swap from './Swap';
 import { ethers } from 'ethers';
 
 export {
   ethers,
+  Donation,
   Payment,
   Sale,
+  Selector,
   Swap,
-  Selector
 }
