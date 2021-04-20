a4d2678e79d0e1d4600a4642db589ac809195679
Sebastian Pape <spape@users.noreply.github.com>
2021-04-20 12:55:53 +0200

    remove typescript, fix bundle (#9)

--- rollup.serve.config.js
+++ rollup.serve.config.js
@@ -1,34 +1,14 @@
 import commonjs from '@rollup/plugin-commonjs';
-import del from 'rollup-plugin-delete'
 import livereload from 'rollup-plugin-livereload';
-import pkg from './package.json';
 import replace from '@rollup/plugin-replace';
 import resolve from '@rollup/plugin-node-resolve';
 import rollup from './rollup.config.js';
 import serve from 'rollup-plugin-serve';
 import sucrase from '@rollup/plugin-sucrase';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { terser } from "rollup-plugin-terser";
 
-// This creates only development builds used for demo and development
 export default Object.assign({}, rollup, {
-  output: [
-    {
-      format: 'cjs',
-      file: 'tmp/cjs/index.js'
-    },
-    {
-      format: 'es',
-      file: 'tmp/es/index.js'
-    },
-    {
-      format: 'umd',
-      name: pkg.moduleName,
-      file: 'tmp/umd/index.js'
-    },
-  ],
   plugins: [
-    del({ targets: 'tmp/*' }),
     sucrase({
       exclude: ['node_modules/**'],
       transforms: ['typescript', 'jsx']
@@ -41,14 +21,15 @@ export default Object.assign({}, rollup, {
       include: 'node_modules/**'
     }),
     replace({
-      'process.env.NODE_ENV': JSON.stringify( 'production' )
+      'process.env.NODE_ENV': JSON.stringify( 'production' ),
+      preventAssignment: true
     }),
     serve({
       open: 'true',
-      openPage: '/development.html'
+      openPage: '/demo.html'
     }),
     livereload({
-      watch: ['tmp']
+      watch: 'dist'
     })
-  ]
+  ],
 })
