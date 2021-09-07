58958fb03f55c1776755c516075facc1b049c9b7
Sebastian Pape <s@pape.io>
2021-09-07 20:24:41 +0200

    add filter example

--- dist/es/index.js
+++ dist/es/index.js
@@ -1,3 +1,5 @@
+
+(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
 import React, { useState, useContext, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { ReactShadowDOM } from 'depay-react-shadow-dom';
