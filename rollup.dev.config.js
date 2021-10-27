1ed111d50cf27d45bc0d3f55449da847654f4ec2
Sebastian Pape <s@pape.io>
2021-10-27 17:02:07 +0200

    development improvements

--- rollup.dev.config.js
+++ rollup.dev.config.js
@@ -1,8 +1,18 @@
+import globals from './rollup.globals.js'
 import livereload from 'rollup-plugin-livereload'
+import pkg from './package.json'
 import rollup from './rollup.module.config.js'
 import serve from 'rollup-plugin-serve'
 
 export default Object.assign({}, rollup, {
+  output: [
+    {
+      format: 'umd',
+      name: pkg.moduleName,
+      globals: globals,
+      file: 'tmp/index.dev.js'
+    },
+  ],
   plugins: [...rollup.plugins,
     serve({
       open: 'true',
