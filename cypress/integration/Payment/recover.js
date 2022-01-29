87b551edb76331149dec1bf327d306494d232054
Sebastian Pape <s@pape.io>
2022-01-29 15:27:34 +0100

    continue specs for recover

---
+++ cypress/integration/Payment/recover.js
@@ -0,0 +1,187 @@
+import DePayWidgets from '../../../src'
+import fetchMock from 'fetch-mock'
+import mockBasics from '../../../tests/mocks/basics'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { CONSTANTS } from '@depay/web3-constants'
+import { mock, confirm, fail, increaseBlock, resetMocks, replace } from '@depay/web3-mock'
+import { resetCache, provider } from '@depay/web3-client'
+import { routers, plugins } from '@depay/web3-payments'
+import { Server } from 'mock-socket'
+import { Token } from '@depay/web3-tokens'
+
+describe('recovers a previously made payment transaction', () => {
+
+  const blockchain = 'ethereum'
+  const accounts = ['0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045']
+  beforeEach(resetMocks)
+  beforeEach(resetCache)
+  beforeEach(()=>fetchMock.restore())
+  beforeEach(()=>mock({ blockchain, accounts: { return: accounts } }))
+
+  let DEPAY = '0xa0bEd124a09ac2Bd941b10349d8d224fe3c955eb'
+  let DAI = CONSTANTS[blockchain].USD
+  let ETH = CONSTANTS[blockchain].NATIVE
+  let WETH = CONSTANTS[blockchain].WRAPPED
+  let fromAddress = accounts[0]
+  let toAddress = '0x4e260bB2b25EC6F3A59B478fCDe5eD5B8D783B02'
+  let amount = 20
+  let TOKEN_A_AmountBN
+
+  beforeEach(()=>{
+
+    ({ TOKEN_A_AmountBN } = mockBasics({
+      provider: provider(blockchain),
+      blockchain,
+
+      fromAddress,
+      fromAddressAssets: [
+        {
+          "name": "Ether",
+          "symbol": "ETH",
+          "address": ETH,
+          "type": "NATIVE"
+        }, {
+          "name": "Dai Stablecoin",
+          "symbol": "DAI",
+          "address": DAI,
+          "type": "20"
+        }, {
+          "name": "DePay",
+          "symbol": "DEPAY",
+          "address": DEPAY,
+          "type": "20"
+        }
+      ],
+      
+      toAddress,
+
+      exchange: 'uniswap_v2',
+      NATIVE_Balance: 0,
+
+      TOKEN_A: DEPAY,
+      TOKEN_A_Decimals: 18,
+      TOKEN_A_Name: 'DePay',
+      TOKEN_A_Symbol: 'DEPAY',
+      TOKEN_A_Amount: amount,
+      TOKEN_A_Balance: 30,
+      
+      TOKEN_B: DAI,
+      TOKEN_B_Decimals: 18,
+      TOKEN_B_Name: 'Dai Stablecoin',
+      TOKEN_B_Symbol: 'DAI',
+      TOKEN_B_Amount: 33,
+      TOKEN_B_Balance: 50,
+
+      TOKEN_A_TOKEN_B_Pair: CONSTANTS[blockchain].ZERO,
+      TOKEN_B_WRAPPED_Pair: '0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11',
+      TOKEN_A_WRAPPED_Pair: '0xEF8cD6Cb5c841A4f02986e8A8ab3cC545d1B8B6d',
+
+      WRAPPED_AmountIn: 0.01,
+      USD_AmountOut: 33,
+
+      timeZone: 'Europe/Berlin',
+      stubTimeZone: (timeZone)=> {
+        cy.stub(Intl, 'DateTimeFormat', () => {
+          return { resolvedOptions: ()=>{
+            return { timeZone }
+          }}
+        })
+      },
+
+      currency: 'EUR',
+      currencyToUSD: '0.85'
+    }))
+  })
+
+  describe('confirmed via websockets', ()=>{
+    let mockedWebsocketServer = new Server('wss://integrate.depay.fi/cable')
+    let websocketMessages = []
+    let mockedWebsocket
+
+    beforeEach(()=>{
+      mockedWebsocketServer.on('connection', socket => {
+        mockedWebsocket = socket
+        mockedWebsocket.on('message', data => {
+          websocketMessages.push(data)
+        })
+      })
+    })
+
+    it.only('recovers a previously made payment transaction and confirms it via websockets', () => {
+      let transaction = {
+        from: fromAddress,
+        to: DEPAY,
+        api: Token[blockchain].DEFAULT,
+        method: 'transfer',
+        params: [toAddress, TOKEN_A_AmountBN]
+      }
+      
+      let transactionId = '0x081ae81229b2c7df586835e9e4c16aa89f8a15dc118fac31b7521477c53ed2a9'
+      let transactionNonce = 2865
+      let transactionAfterBlock = 14088130
+
+      fetchMock.get({
+        url: `https://api.depay.fi/v2/transactions/${blockchain}/${fromAddress.toLowerCase()}/${transactionNonce}`,
+        overwriteRoutes: true
+      }, { status: 404 })
+
+      cy.visit('cypress/test.html').then((contentWindow) => {
+        cy.document().then((document)=>{
+          DePayWidgets.Payment({
+            recover: {
+              blockchain: blockchain,
+              transaction: transactionId,
+              sender: transaction.from,
+              nonce: transactionNonce,
+              afterBlock: transactionAfterBlock,
+              token: DEPAY,
+              amount: amount
+            },
+            confirmed: ()=> {
+              console.log('confirmed')
+            },
+            document
+          })
+          cy.get('button[title="Close dialog"]', { includeShadowDom: true }).should('not.exist')
+          cy.get('.ReactShadowDOMOutsideContainer').shadow().find('.ButtonPrimary').should('contain.text', 'Paying...')
+          cy.get('.ReactShadowDOMOutsideContainer').shadow().find('.ButtonPrimary').invoke('attr', 'href').should('include', 'https://etherscan.io/tx/'+transactionId)
+          cy.wait(2000).then(()=>{
+            expect(!!websocketMessages.find((rawMessage)=>{
+              let message = JSON.parse(rawMessage)
+              return(
+                message.command == 'subscribe' &&
+                message.identifier == JSON.stringify({ blockchain, sender: fromAddress.toLowerCase(), nonce: transactionNonce, channel: 'TransactionChannel' })
+              )
+            })).to.equal(true)
+            let replacingTransactionId = '0x782cf9983541087548c717dc1a4e2687ef8928e758316cd600ebb0652f57bafe'
+            mockedWebsocket.send(JSON.stringify({
+              message: {
+                id: transactionId,
+                status: 'success'
+              }
+            }))
+          })
+          cy.wait(1000).then(()=>{
+            cy.get('.ReactShadowDOMOutsideContainer').shadow().find('.Card .Checkmark')
+            cy.get('.ReactShadowDOMOutsideContainer').shadow().contains('.Card', 'Payment has been confirmed').invoke('attr', 'href').should('include', `https://etherscan.io/tx/${transactionId}`)
+            cy.get('.ReactShadowDOMOutsideContainer').shadow().find('.Card.disabled').then(()=>{
+              cy.get('button[title="Close dialog"]', { includeShadowDom: true }).should('exist')
+              cy.get('.ReactShadowDOMOutsideContainer').shadow().find('.ButtonPrimary').click()
+              cy.get('.ReactShadowDOMOutsideContainer').should('not.exist')
+            })
+          })
+        })
+      })
+    })
+
+    it('recovers a previously made payment transaction and fails it via websockets', () => {
+    })
+  })
+  
+  it('recovers a previously made payment transaction and confirms it via polling', () => {
+  })
+
+  it('recovers a previously made payment transaction and fails it via polling', () => {
+  })
+})
