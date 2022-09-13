6f12489eff53e39312d40f408a3db14b1fda9825
Sebastian Pape <spape@users.noreply.github.com>
2022-09-13 20:34:23 +0200

    improve auto slippage, adjust rounding precision (#67)
    * improve auto slippage, adjust rounding precision
    
    * fix Payment/auto-slippage

--- src/helpers/round.js
+++ src/helpers/round.js
@@ -1,52 +1,52 @@
 export default (input, direction = 'up')=>{
   let inputAsFloat = parseFloat(input)
 
-  let digitsAfterDecimal = inputAsFloat.toString().match(/\d+\.0*(\d{5})/)
+  let digitsAfterDecimal = inputAsFloat.toString().match(/\d+\.0*(\d{4})/)
 
   if(digitsAfterDecimal?.length) {
     digitsAfterDecimal = digitsAfterDecimal[0]
-    let focus = digitsAfterDecimal.match(/\d{5}$/)[0]
+    let focus = digitsAfterDecimal.match(/\d{4}$/)[0]
     let float 
     let focusToFixed
     if(focus.match(/^0/)) {
       if(direction == 'up') {
-        float = parseFloat(`${focus[1]}.${focus[2]}${focus[3]}${focus[4]}`)
+        float = parseFloat(`${focus[1]}.${focus[2]}${focus[3]}`)
       } else {
-        float = parseFloat(`${focus[1]}.${focus[2]}${focus[3]}${focus[4]}`)
+        float = parseFloat(`${focus[1]}.${focus[2]}${focus[3]}`)
       }
-      focusToFixed = parseFloat(float).toFixed(3)
+      focusToFixed = parseFloat(float).toFixed(2)
       focusToFixed = `0${focusToFixed}`.replace('.', '')
     } else {
       if(direction == 'up') {
-        float = parseFloat(`${focus[0]}.${focus[1]}${focus[2]}${focus[3]}9`)
+        float = parseFloat(`${focus[0]}.${focus[1]}${focus[2]}9`)
       } else {
-        float = parseFloat(`${focus[0]}.${focus[1]}${focus[2]}${focus[3]}1`)
+        float = parseFloat(`${focus[0]}.${focus[1]}${focus[2]}1`)
       }
-      focusToFixed = parseFloat(float).toFixed(3).replace('.', '')
+      focusToFixed = parseFloat(float).toFixed(2).replace('.', '')
     }
-    if(focusToFixed == '09999' && parseInt(inputAsFloat.toFixed(0)) == 0) {
-      focusToFixed = direction == 'up' ? '10000' : '09999'
+    if(focusToFixed == '0999' && parseInt(inputAsFloat.toFixed(0)) == 0) {
+      focusToFixed = direction == 'up' ? '1000' : '0999'
+      return(
+        parseFloat(
+          digitsAfterDecimal.replace(/\d{4}$/, focusToFixed)
+        )
+      )
+    } else if(focusToFixed == '1000' && parseInt(inputAsFloat.toFixed(0)) == 0) {
       return(
         parseFloat(
           digitsAfterDecimal.replace(/\d{5}$/, focusToFixed)
         )
       )
-    } else if(focusToFixed == '10000' && parseInt(inputAsFloat.toFixed(0)) == 0) {
-      return(
-        parseFloat(
-          digitsAfterDecimal.replace(/\d{6}$/, focusToFixed)
-        )
-      )
-    } else if(focusToFixed.toString()[0] != "0" && focusToFixed.toString().length > 4) {
+    } else if(focusToFixed.toString()[0] != "0" && focusToFixed.toString().length > 3) {
       return(parseInt(inputAsFloat.toFixed(0)))
     } else {
       return(
         parseFloat(
-          digitsAfterDecimal.replace(/\d{5}$/, focusToFixed)
+          digitsAfterDecimal.replace(/\d{4}$/, focusToFixed)
         )
       )
     }
   } else {
-    return(parseFloat(inputAsFloat.toFixed(4)))
+    return(parseFloat(inputAsFloat.toFixed(3)))
   }
 }
