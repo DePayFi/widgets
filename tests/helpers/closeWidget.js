c5f5a068c6b79f756c2e13dc159467700f845746
Sebastian Pape <s@pape.io>
2022-10-25 17:22:10 +0200

    wait close widget

--- tests/helpers/closeWidget.js
+++ tests/helpers/closeWidget.js
@@ -1,5 +1,5 @@
 export default ()=> {
-  cy.wait(500).then(()=>{
+  cy.wait(1500).then(()=>{
     cy.get('body').then((body) => {
       if (body.find('.ReactShadowDOMOutsideContainer').length > 0) {
         cy.get('.ReactShadowDOMOutsideContainer').shadow().find('button[title="Close dialog"]').click()
