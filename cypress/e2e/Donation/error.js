05fddc9db7bf0fe8a893e88c29657e6a5b1d39d9
Sebastian Pape <s@pape.io>
2023-02-23 10:37:54 +0100

    fix error

--- cypress/e2e/Donation/error.js
+++ cypress/e2e/Donation/error.js
@@ -171,6 +171,7 @@ describe('Donation Widget: error', () => {
             passedError = error
           }
         })
+        cy.get('.ReactShadowDOMOutsideContainer').shadow().find('.Card').contains('Detected').click()
         cy.get('.ReactShadowDOMOutsideContainer').shadow().contains('h1', 'Oops, Something Went Wrong')
         cy.get('.ReactShadowDOMOutsideContainer').shadow().contains('.ErrorSnippetText', /something failed/)
         cy.get('.ReactShadowDOMOutsideContainer').shadow().contains('strong', 'If this keeps happening, please report it.')
