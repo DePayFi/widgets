d0be17caa2532959bdebc17a0301657300d0423a
Sebastian Pape <s@pape.io>
2023-05-24 10:38:55 +0200

    fix tests

--- cypress/e2e/Payment/main.js
+++ cypress/e2e/Payment/main.js
@@ -276,6 +276,30 @@ describe('Payment Widget: main functionality', () => {
       }
     })
 
+    fetchMock.post({
+      url: "https://public.depay.com/payments",
+      body: {
+        after_block: "1",
+        amount: "20.0",
+        blockchain: "ethereum",
+        confirmations: 1,
+        fee_amount: null,
+        fee_receiver: null,
+        nonce: "0",
+        payload: {
+          sender_amount: "20.0",
+          sender_id: fromAddress,
+          sender_token_id: DEPAY,
+          type: 'payment'
+        },
+        receiver: toAddress,
+        sender: fromAddress,
+        token: DEPAY,
+        transaction: mockedTransaction.transaction._id,
+        uuid: mockedTransaction.transaction._id,
+      },
+    }, 201)
+
     cy.visit('cypress/test.html').then((contentWindow) => {
       cy.document().then((document)=>{
         DePayWidgets.Payment({ ...defaultArguments, document })
@@ -328,9 +352,8 @@ describe('Payment Widget: main functionality', () => {
         receiver: toAddress,
         sender: fromAddress,
         token: DEPAY,
-        transaction: mockedTransaction.transaction._id,
-        uuid: mockedTransaction.transaction._id,
       },
+      matchPartialBody: true
     }, 201)
 
     cy.visit('cypress/test.html').then((contentWindow) => {
