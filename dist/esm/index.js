cfbcd872baac5f8f1a13428be080f5c9dd5ed1d7
Sebastian Pape <0xneo11@gmail.com>
2024-11-08 18:41:31 +0100

    fix solflare deeplink

--- dist/esm/index.js
+++ dist/esm/index.js
@@ -387,7 +387,7 @@ var allWallets = [{
     "ios": {
       "native": "solflare:",
       "open": function open() {
-        return "https://solflare.com/ul/browse/".concat(encodeURIComponent(window.location.toString()), "?ref=").concat(encodeURIComponent(window.location.origin.toString()));
+        return "solflare:://ul/v1/browse/".concat(encodeURIComponent(window.location.toString()), "?ref=").concat(encodeURIComponent(window.location.origin.toString()));
       },
       "universal": "https://solflare.com/ul",
       "solanaPay": true
